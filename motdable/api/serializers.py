5cf4def003ebfa0b89e13a425321553377c428c8
Brice Burgess <briceburg@gmail.com>
2013-09-16 16:34:04 -0500

    Removed explicit field handling for relationships in serializers [now automatic, see: http://django-rest-framework.org/topics/2.3-announcement.html#easier-serializers ]

--- motdable/api/serializers.py
+++ motdable/api/serializers.py
@@ -1,29 +1,24 @@
 from rest_framework import serializers
 
-from api.models import Player, PlayCall
+from api.models import Player, PlayCall, PlayCallOption
 from django.contrib.auth.models import User
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.Field(source='owner.username')
-   
+
     class Meta:
         model = Player
         fields = ('url', 'id', 'hostname', 'login_username', 'login_private_key', 'owner')
         
-
+        
 class PlayCallSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.Field(source='owner.username')
    
     class Meta:
         model = PlayCall
-        fields = ('url', 'id', 'title', 'playbook', 'owner')
-        
-        
+        fields = ('url', 'id', 'title', 'playbook', 'options', 'owner')
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    #playcalls = serializers.PrimaryKeyRelatedField(many=True)
-    playcalls = serializers.HyperlinkedRelatedField(many=True, view_name='playcall-detail')
-    players = serializers.HyperlinkedRelatedField(many=True, view_name='player-detail')
     
     class Meta:
         model = User
