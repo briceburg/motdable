0448b63809601789161c78794d97f242e4347efa
Brice Burgess <briceburg@gmail.com>
2013-09-17 00:56:35 -0500

    pass extravars to ansible-playbook

--- motdable/coordinator/views.py
+++ motdable/coordinator/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from __future__ import print_function
 
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.utils import simplejson
-import subprocess
+from api.models import PlayCall, Player
+
+import subprocess, sys, json, os
+
 
 def index(request):
     return render(request, 'coordinator/index.html', 
@@ -14,24 +17,55 @@ def catchall(request):
 
 def execute(request):
     
-    options     = request.GET.get('options',[])
-    playcall    = request.GET['playcallId']
-    player      = request.GET['playerId']
+    options     = request.GET.get('options','{}')
+    playcall    = PlayCall.objects.get(pk=request.GET['playcallId'])
+    player      = Player.objects.get(pk=request.GET['playerId'])
     
-    # @todo playbook execution should be a celery task / non-blocking call.
-    mock_output = subprocess.check_output(["ls", "-l"]) 
-    
-    
-    result = {
-              "options": options,
-              "playcall": playcall, 
-              "player": player, 
-              "output": mock_output
-    }
-    
-    
-    return HttpResponse(simplejson.dumps(result), 
+    if hasattr(sys, 'real_prefix'): 
+        
+        # write a temp inventory file, @todo: one-liner?
+        inventory_file = sys.prefix + '/tmp.inventory' 
+        f = open(inventory_file,'w')
+        print('127.0.0.1', file=f)
+        print(player.hostname, file=f)
+        f.close()
+        
+        # write a temp playbook file, @todo: one-liner?
+        playbook_file = sys.prefix + '/tmp.playbook' 
+        f = open(playbook_file,'w')
+        print(playcall.playbook, file=f)
+        f.close() 
+        
+        # create extravars
+        extra_vars = json.loads(options)
+        extra_vars['host_group'] = player.hostname
+        
+        env_vars = dict(os.environ)
+        env_vars['ANSIBLE_HOST_KEY_CHECKING'] = 'False'
+        
+        # @todo playbook execution should be a celery task / non-blocking call.
+        process = subprocess.Popen([
+            sys.prefix + '/bin/ansible-playbook',
+            '--inventory-file=' + inventory_file,
+            '--user=' + player.login_username,
+            '--private-key=' + player.login_private_key,
+            '--extra-vars=' + json.dumps(extra_vars),
+            playbook_file
+        
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env_vars)
+        
+        stdout, stderr = process.communicate()
+        output = stderr if stderr else stdout
+        
+    else:
+        # note: it's irrelevant if we're within a virtualenv or not,
+        #  I am just using as an indicator that the ansible dependencies
+        #  have been installed as per installation instructions requirements.
+        #    ++ as a programming exercise to detect a virtual environment.
+        output  = 'Failed to execute. Django not running in a virtualenv.'
+            
+
+    return HttpResponse(json.dumps({"output": output}),
                         mimetype='application/json')
-    
     
     
